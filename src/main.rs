153b53e5c6dd48e4ba7b7808e6f814e8fc5e11ea
kickhead13 <75039938+kickhead13@users.noreply.github.com>
2023-04-29 19:37:54 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod simulator;
+use simulator::percolation;
+
+#[allow(unused_imports)]
+use rand::prelude::*;
+
+fn main() {
+   let mut g = percolation::Grid::new(100, 100);
+   g.cli_display_diagram_shortened();
+   g.percolate(10, 100, true);
+   println!();
+   println!();
+  println!();println!();
+   g.cli_display_diagram_shortened();
+}
